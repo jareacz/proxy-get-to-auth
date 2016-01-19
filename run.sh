33bc858a37dd2f5811c73859132b8ac8aa5a2c41
Ludek Vesely <ludek@slever.cz>
2016-01-19 14:23:27 +0100

    removed demonize configuration

--- run.sh
+++ run.sh
@@ -3,9 +3,6 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-# make sure it doesn't daemonize
-echo "daemon off;" >> /etc/nginx/nginx.conf
-
 # replace variables at runtime
 sed -i "s/SERVER_NAME/${SERVER_NAME}/g" /etc/nginx/conf.d/proxy.conf
 sed -i "s/PROTO/${PROTO}/g" /etc/nginx/conf.d/proxy.conf
